61b7a2651fa7e915157d709f84f8a008cd1aea73
emanuel <amoz_c.p.t@hotmail.com>
2023-09-04 22:53:04 -0300

    Create login_page class

---
+++ lib/src/pages/login/login_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<LoginPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<LoginPage> {
+  // int _counter = 0;
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            )),
+        const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Senha'),
+            )),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              margin: const EdgeInsets.all(7.0),
+              decoration: BoxDecoration(
+                // border: Border.all(),
+                borderRadius: BorderRadius.circular(10),
+                gradient: const LinearGradient(
+                  colors: <Color>[
+                    Color(0xFFD16BA5),
+                    Color(0xFF86a8e7),
+                    Color(0xFF5ffbf1),
+                  ],
+                ),
+              ),
+              child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  )),
+            ),
+            TextButton(
+              onPressed: () {
+                // Navigator.of(context).push(MaterialPageRoute(
+                //     builder: (context) => RegisterPage()));
+              },
+              child: const Text(
+                'Cadastrar',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+}
