1fa96cd592b07c6d242118cec011e65266853cf6
emanuel <amoz_c.p.t@hotmail.com>
2023-09-05 00:10:33 -0300

    Code Refactoring and Navigator to BookContent

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:catalogo_de_livros/src/components/card_livro.dart';
-import 'package:catalogo_de_livros/src/models/livros.dart';
+import 'package:catalogo_de_livros/src/components/card_book.dart';
+import 'package:catalogo_de_livros/src/models/books.dart';
+import 'package:catalogo_de_livros/src/pages/book_content/book_content.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,53 +14,44 @@ class HomePage extends StatelessWidget {
         title: const Text('Catálogo de Livros'),
       ),
       body: ListView.builder(
-          itemCount: catalogoDeLivros.length,
+          itemCount: booksCatalogue.length,
           itemBuilder: (context, index) {
             return GestureDetector(
-                onTap: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => ConteudoLivro(),
-                  //   )
-                  // )
-                },
-                child: CardLivro(livros: catalogoDeLivros[index]));
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BookContent(booksCatalogue[index]),
+                    ));
+              },
+              child: CardBook(livros: booksCatalogue[index]),
+            );
           }),
     );
   }
 }
 
-List<Livro> catalogoDeLivros = [
-  Livro(
-    titulo:
-        'Desenvolvedora de jogos polêmicos libera pirataria de seus games; entenda',
-    autor:
-        'A desenvolvedora do polêmico jogo Postal publicou nas redes sociais que os gamers estão liberados para piratear seus jogos.',
-    genero: 'https://tm.ibxk.com.br/2023/08/14/14143927728256.jpg?ims=704x264',
-    anoDePublicacao: 2000,
+List<Book> booksCatalogue = [
+  Book(
+    title: 'Introdução ao Pentest',
+    autor: 'Daniel Moreno',
+    genre: 'Computação e Tecnologia',
+    publicationYear: 2019,
   ),
-  Livro(
-      titulo: "Baldur's Gate 3: entenda a origem do nome do jogo",
-      autor:
-          "Baldur's Gate 3 foi lançado dia 3 de agosto e está recebendo uma enxurrada de elogios, inclusive sendo um forte candidato a jogo do ano.",
-      genero:
-          'https://tm.ibxk.com.br/2023/08/15/15181341512388.jpg?ims=704x264',
-      anoDePublicacao: 2000),
-  Livro(
-      titulo:
-          'Modern Warfare 3 traz de volta o minimapa clássico de Call of Duty',
-      autor:
-          'As mudanças para o multijogador de Modern Warfare 3 que agradam ao público continuam chegando.',
-      genero:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.wccftech.com%2Fwp-content%2Fuploads%2F2020%2F04%2Fcall-of-duty-MW3-remastered.jpg&f=1&nofb=1&ipt=709dcc6d6c1867f0e2f3b1d3998c611bd40fd04064faf481dd549dea2a010396&ipo=images',
-      anoDePublicacao: 2000),
-  Livro(
-      titulo:
-          "Jogadores de Diablo 4 estão usando masmorra apelidada de 'túneis de dopamina' para subir 40 níveis em 2 horas",
-      autor:
-          'Os jogadores de Diablo 4 estão explorando a fundo uma masmorra tão cheia de monstros que permite que eles ganhem 40 níveis em apenas duas horas.',
-      genero:
-          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F4078681.jpg&f=1&nofb=1&ipt=3ea2c6fd51aa54876da56f46ff6f9cb057969ff45257151d3f5ac6e7d4617bb9&ipo=images',
-      anoDePublicacao: 2000)
+  Book(
+      title:
+          "Técnicas de Invasão: Aprenda as técnicas usadas por hackers em invasões reais ",
+      autor: "Bruno Fraga",
+      genre: 'Computação e Tecnologia',
+      publicationYear: 2019),
+  Book(
+      title: 'A Arte da Guerra: Edição Especial ',
+      autor: 'Sun Tzu',
+      genre: 'Autodesenvolvimento',
+      publicationYear: 2021),
+  Book(
+      title: "It: A coisa ",
+      autor: "Stephen King",
+      genre: "Terror",
+      publicationYear: 2014)
 ];
