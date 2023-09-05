84d7c6c4ebb054bc33872a9aabb573a3f66c5754
emanuel <amoz_c.p.t@hotmail.com>
2023-09-04 23:41:01 -0300

    Create model Livro

---
+++ lib/src/models/livros.dart
@@ -0,0 +1,12 @@
+class Livro {
+  final String titulo;
+  final String autor;
+  final String genero;
+  final int anoDePublicacao;
+
+  Livro(
+      {required this.titulo,
+      required this.autor,
+      required this.genero,
+      required this.anoDePublicacao});
+}
