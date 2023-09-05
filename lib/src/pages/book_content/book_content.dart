a186f0834b3d9d408aa89706711f7cb53f9cd6cb
emanuel <amoz_c.p.t@hotmail.com>
2023-09-05 01:04:08 -0300

    Update EditBook FloatingButton

--- lib/src/pages/book_content/book_content.dart
+++ lib/src/pages/book_content/book_content.dart
@@ -1,16 +1,31 @@
 import 'package:catalogo_de_livros/src/models/books.dart';
 import 'package:flutter/material.dart';
 
-class BookContent extends StatelessWidget {
+import '../add_book/edit_book.dart';
+
+class BookContent extends StatefulWidget {
   final Book books;
 
   BookContent(this.books);
 
   @override
+  State<BookContent> createState() => _BookContentState();
+}
+
+_openEditBookModal(BuildContext context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return const EditBook();
+      });
+}
+
+class _BookContentState extends State<BookContent> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(books.title),
+        title: Text(widget.books.title),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Padding(
@@ -25,12 +40,17 @@ class BookContent extends StatelessWidget {
             )),
             const SizedBox(height: 16.0),
             Text(
-                "Ano de Publicação do livro: ${books.publicationYear.toString()}"),
-            Text("Autor do livro: ${books.autor}"),
-            Text("Gênero do livro: ${books.genre}")
+                "Ano de Publicação do livro: ${widget.books.publicationYear.toString()}"),
+            Text("Autor do livro: ${widget.books.autor}"),
+            Text("Gênero do livro: ${widget.books.genre}")
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openEditBookModal(context),
+        child: const Icon(Icons.edit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
