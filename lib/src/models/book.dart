24b7cfe1ea81ecc36854e89d1809473660376e33
emanuel <amoz_c.p.t@hotmail.com>
2023-09-05 02:57:44 -0300

    Add provider package

---
+++ lib/src/models/book.dart
@@ -0,0 +1,12 @@
+class Book {
+  final String title;
+  final String autor;
+  final String genre;
+  final num publicationYear;
+
+  Book(
+      {required this.title,
+      required this.autor,
+      required this.genre,
+      required this.publicationYear});
+}
