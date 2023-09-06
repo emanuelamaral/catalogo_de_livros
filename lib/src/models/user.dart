3931af9217c9fc9d9bb285592f09c52794f5bb90
emanuel <amoz_c.p.t@hotmail.com>
2023-09-05 23:48:33 -0300

    Create UserModel

---
+++ lib/src/models/user.dart
@@ -0,0 +1,15 @@
+class UserModel {
+  final int id;
+  final String email;
+  final String password;
+
+  UserModel({required this.id, required this.email, required this.password});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'password': password,
+    };
+  }
+}
