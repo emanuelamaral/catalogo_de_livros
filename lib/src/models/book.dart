b9c33678025c387fe09b54ace18a039407fe0255
emanuel <amoz_c.p.t@hotmail.com>
2023-09-06 18:25:42 -0300

    Update non-nullable id

--- lib/src/models/book.dart
+++ lib/src/models/book.dart
@@ -1,12 +1,24 @@
 class Book {
+  final int? id;
   final String title;
   final String autor;
   final String genre;
   final num publicationYear;
 
   Book(
-      {required this.title,
+      {this.id,
+      required this.title,
       required this.autor,
       required this.genre,
       required this.publicationYear});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'autor': autor,
+      'genre': genre,
+      'publication_year': publicationYear
+    };
+  }
 }
--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,9 +1,9 @@
 class UserModel {
-  final int id;
+  final int? id;
   final String email;
   final String password;
 
-  UserModel({required this.id, required this.email, required this.password});
+  UserModel({this.id, required this.email, required this.password});
 
   Map<String, dynamic> toMap() {
     return {
