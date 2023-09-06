76877b662fd04d14891d39c00175dc65aa7f0d21
emanuel <amoz_c.p.t@hotmail.com>
2023-09-06 18:26:53 -0300

    Add Book using sqlite

--- lib/src/pages/add_book/add_book.dart
+++ lib/src/pages/add_book/add_book.dart
@@ -1,4 +1,5 @@
 import 'package:catalogo_de_livros/src/models/book.dart';
+import 'package:catalogo_de_livros/src/repositories/book_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,18 +14,46 @@ class AddBook extends StatefulWidget {
 }
 
 class _AddBookState extends State<AddBook> {
-  Book? book;
+  // Book? book;
 
-  _addBook(title, String autor, String genre, num publicationYear) {
-    final newBook = Book(
-        title: title,
-        autor: autor,
-        genre: genre,
-        publicationYear: publicationYear);
+  // _addBook(title, String autor, String genre, num publicationYear) {
+  //   final newBook = Book(
+  //       title: title,
+  //       autor: autor,
+  //       genre: genre,
+  //       publicationYear: publicationYear);
 
-    setState(() {
-      book = newBook;
-    });
+  //   setState(() {
+  //     book = newBook;
+  //   });
+  // }
+
+  final titleController = TextEditingController();
+  final autorController = TextEditingController();
+  final genreController = TextEditingController();
+  final publicationYearController = TextEditingController();
+
+  void _submitForm(BuildContext context) async {
+    final title = titleController.text;
+    final autor = autorController.text;
+    final genre = genreController.text;
+    final publicationYear = int.tryParse(publicationYearController.text) ?? 0.0;
+
+    if (title.isEmpty ||
+        autor.isEmpty ||
+        genre.isEmpty ||
+        publicationYear < 0) {
+      return;
+    }
+
+    final bookRepository = Provider.of<BookRepository>(context, listen: false);
+    dynamic bookId =
+        await bookRepository.addBook(title, autor, genre, publicationYear);
+
+    if (bookId != null) {
+      print('Livro Cadastrado');
+      Navigator.of(context).pop();
+    }
   }
 
   @override
@@ -37,7 +66,50 @@ class _AddBookState extends State<AddBook> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          BookForm(onSubmit: _addBook),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: titleController,
+                  onSubmitted: (_) => _submitForm,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Titulo do livro'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: autorController,
+                  onSubmitted: (_) => _submitForm,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Nome do Autor'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: genreController,
+                  onSubmitted: (_) => _submitForm,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Gênero do livro'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: publicationYearController,
+                  onSubmitted: (_) => _submitForm,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Ano de Publicação'),
+                ),
+              ),
+            ],
+          ),
           Container(
             width: double.infinity,
             margin: const EdgeInsets.all(8.0),
@@ -52,14 +124,7 @@ class _AddBookState extends State<AddBook> {
               ),
             ),
             child: TextButton(
-                onPressed: () {
-                  final bookList = context.read<BookList>();
-
-                  if (book != null) {
-                    bookList.addBook(book!);
-                  }
-                  Navigator.of(context).pop();
-                },
+                onPressed: () => _submitForm(context),
                 child: const Text('Cadastrar Livro',
                     style: TextStyle(fontSize: 18, color: Colors.white))),
           )
