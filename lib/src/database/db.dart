6b4e9b496916dac51a5b59795599246a1a73a959
emanuel <amoz_c.p.t@hotmail.com>
2023-09-06 18:25:23 -0300

    Update DB class

--- lib/src/database/db.dart
+++ lib/src/database/db.dart
@@ -8,38 +8,51 @@ class DB {
   static Database? _database;
 
   Future<Database> get database async {
-    if (_database != null) return _database!;
-    return await _initDatabase();
+    if (_database == null) {
+      await _initDatabase();
+    }
+    return _database!;
   }
 
-  Future<Database> _initDatabase() async {
-    return await openDatabase(
-      join(await getDatabasesPath(), 'catalogo_de_livro.db'),
+  Future<void> _initDatabase() async {
+    final path = await getDatabasesPath();
+    final databasePath = join(path, 'catalogo_de_livro4.db');
+
+    if (await databaseExists(databasePath)) {
+      await deleteDatabase(databasePath);
+    }
+
+    // return await openDatabase(
+    //   join(path, 'catalogo_de_livro4.db'),
+    //   version: 1,
+    //   onCreate: _onCreate,
+    //   // onUpgrade: _onUpgrade,
+    // );
+
+    _database = await openDatabase(
+      databasePath,
       version: 1,
       onCreate: _onCreate,
     );
   }
 
+  // Future<void> deleteDatabase() async {
+  //   final path = await getDatabasesPath();
+  //   final databasePath = join(path, 'catalogo_de_livro4.db');
+  //   await deleteDatabase(databasePath);
+  //   _database = null;
+  // }
+
   Future<void> _onCreate(Database db, int version) async {
-    await db.execute(_usuario);
-    await db.execute(_livro);
+    await db.execute(" CREATE TABLE tb_usuario ( "
+        " id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        " email TEXT NOT NULL, "
+        " password TEXT NOT NULL) ");
+    await db.execute("CREATE TABLE tb_livro ( "
+        " id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        " title TEXT NOT NULL, "
+        " autor TEXT NOT NULL, "
+        " genre TEXT NOT NULL, "
+        " publication_year INTEGER NOT NULL) ");
   }
-
-  String get _usuario => '''
-    CREATE TABLE tb_usuario (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email TEXT NOT NULL,
-      password TEXT NOT NULL
-    );
-  ''';
-
-  String get _livro => ''' 
-    CREATE TABLE tb_livro (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      titulo TEXT NOT NULL,
-      autor TEXT NOT NULL,
-      genero TEXT NOT NULL,
-      ano_de_publicacao INTEGER NOT NULL
-    )
-  ''';
 }
