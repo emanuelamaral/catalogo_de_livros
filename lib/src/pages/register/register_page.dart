97f99b74f93a7a3ed892b1329b513a8cc71a9b7c
emanuel <amoz_c.p.t@hotmail.com>
2023-09-05 23:49:18 -0300

    Create user persistence

--- lib/src/pages/register/register_page.dart
+++ lib/src/pages/register/register_page.dart
@@ -1,7 +1,33 @@
+import 'package:catalogo_de_livros/src/repositories/user_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void _submitForm(BuildContext context) async {
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      return;
+    }
+
+    final userRepository = Provider.of<UserRepository>(context, listen: false);
+    dynamic userId = await userRepository.addUser(email, password);
+
+    if (userId != null) {
+      print('Usuário cadastrado com sucesso!');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,25 +39,27 @@ class RegisterPage extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: emailController,
+                decoration: const InputDecoration(
                     border: OutlineInputBorder(), labelText: 'Email'),
                 keyboardType: TextInputType.emailAddress,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
+                controller: passwordController,
                 obscureText: true,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                     border: OutlineInputBorder(), labelText: 'Senha'),
               ),
             ),
             Container(
               width: double.infinity,
-              margin: EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
                 gradient: const LinearGradient(
@@ -43,7 +71,7 @@ class RegisterPage extends StatelessWidget {
                 ),
               ),
               child: TextButton(
-                  onPressed: () {},
+                  onPressed: () => _submitForm(context),
                   child: const Text('Cadastrar',
                       style: TextStyle(fontSize: 18, color: Colors.white))),
             )
