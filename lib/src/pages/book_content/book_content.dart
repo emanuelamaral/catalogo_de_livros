cf033344087bee39c5ef4b94dc1dfda12402a0df
emanuel <amoz_c.p.t@hotmail.com>
2023-09-07 00:22:03 -0300

    Update modal EditBook

--- lib/src/pages/book_content/book_content.dart
+++ lib/src/pages/book_content/book_content.dart
@@ -1,31 +1,31 @@
 import 'package:catalogo_de_livros/src/models/book.dart';
 import 'package:flutter/material.dart';
 
-import '../add_book/edit_book.dart';
+import '../edit_book/edit_book.dart';
 
 class BookContent extends StatefulWidget {
-  final Book books;
+  final Book book;
 
-  BookContent(this.books);
+  BookContent(this.book);
 
   @override
   State<BookContent> createState() => _BookContentState();
 }
 
-_openEditBookModal(BuildContext context) {
-  showModalBottomSheet(
-      context: context,
-      builder: (_) {
-        return const EditBook();
-      });
-}
-
 class _BookContentState extends State<BookContent> {
+  _openEditBookModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return EditBook(widget.book);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.books.title),
+        title: Text(widget.book.title),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: Padding(
@@ -40,9 +40,9 @@ class _BookContentState extends State<BookContent> {
             )),
             const SizedBox(height: 16.0),
             Text(
-                "Ano de Publicação do livro: ${widget.books.publicationYear.toString()}"),
-            Text("Autor do livro: ${widget.books.autor}"),
-            Text("Gênero do livro: ${widget.books.genre}")
+                "Ano de Publicação do livro: ${widget.book.publicationYear.toString()}"),
+            Text("Autor do livro: ${widget.book.autor}"),
+            Text("Gênero do livro: ${widget.book.genre}")
           ],
         ),
       ),
